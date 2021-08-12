88b97a753c67b6761508323233afbf634c079c7a
Raja R <rraja9513@gmail.com>
2021-08-12 10:39:35 +0530

    additional driver route update route added 12/08/2021

--- routes/enrollfleetowner.js
+++ routes/enrollfleetowner.js
@@ -113,7 +113,86 @@ router.route('/').post((req, res) => {
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
-  
+router.route('/update/:id').post((req,res)=>{
+  Enrollfleetowner.findById(req.params.id)
+    .then(enrollfleetowner => {
+    enrollfleetowner.ofirstname=req.body.ofirstname;
+    enrollfleetowner.omiddlename=req.body.omiddlename;
+    enrollfleetowner.olastname=req.body.olastname;
+    enrollfleetowner.odateofbirth=req.body.odateofbirth;
+    enrollfleetowner.ogender=req.body.ogender;
+    enrollfleetowner.oemail=req.body.oemail;
+    enrollfleetowner.ophonenumber=req.body.ophonenumber;
+    enrollfleetowner.opermanentaddress=req.body.opermanentaddress;
+    enrollfleetowner.opcountry=req.body.opcountry;
+    enrollfleetowner.opstate=req.body.opstate;
+    enrollfleetowner.opcity=req.body.opcity;
+    enrollfleetowner.opzipcode=req.body.opzipcode;
+    enrollfleetowner.ocurrentaddress=req.body.ocurrentaddress;
+    enrollfleetowner.occountry=req.body.occountry;
+    enrollfleetowner.ocstate=req.body.ocstate;
+    enrollfleetowner.occity=req.body.occity;
+    enrollfleetowner.oczipcode=req.body.oczipcode;
+    enrollfleetowner.oadharprooffront=req.body.oadharprooffront;
+    enrollfleetowner.oadharproofback=req.body.oadharproofback;
+    enrollfleetowner.opanproof=req.body.opanproof;
+    enrollfleetowner.vehicledetails=[{
+        vvehiclemodel:req.body.vvehiclemodel,
+        vvehicletype:req.body.vvehicletype,
+        vvehiclecapacity:req.body.vvehiclecapacity,
+        vregistrationnumber:req.body.vregistrationnumber,
+        vchassisnumber:req.body.vchassisnumber,
+        venginenumber:req.body.venginenumber,
+        vrccardnumber:req.body.vrccardnumber,
+        vinsurancenumber:req.body.vinsurancenumber,
+        vemmissiontest:req.body.vemmissiontest,
+        vrccardrenewaldate:req.body.vrccardrenewaldate,
+        vinsurancerenewaldate:req.body.vinsurancerenewaldate,
+        vemmissiontestrenewaldate:req.body.vemmissiontestrenewaldate,
+        vtaxrenewal:req.body.vtaxrenewal,
+        vstatepermit:req.body.vstatepermit,
+        vnationalpermit:req.body.vnationalpermit,
+        vstatepermitdocument:req.body.vstatepermitdocument,
+        vnationalpermitdocument:req.body.vnationalpermitdocument,
+        vinsurancedocument:req.body.vinsurancedocument,
+        vvehiclepicture:req.body.vvehiclepicture,
+        vemissiontestdocument:req.body.vemissiontestdocument,
+        vrccardproof:req.body.vrccardproof,
+        vtaxrenewalproof:req.body.vtaxrenewalproof
+    }];
+    enrollfleetowner.driverdetails=[{
+        ddrivername:req.body.ddrivername,
+        ddriverid:req.body.ddriverid,
+        ddgender:req.body.dgender,
+        dpermanentaddress:req.body.dpermanentaddress,
+        dpresentaddress:req.body.dpresentaddress,
+        ddateofbirth:req.body.ddateofbirth,
+        dmobilenumber:req.body.dmobilenumber,
+        demergencycontactnumber:req.body.demergencycontactnumber,
+        dadharcardnumber:req.body.dadharcardnumber,
+        dinsurancenumber:req.body.dinsurancenumber,
+        ddrivinglicence:req.body.ddrivinglicence,
+        dlanguagesknown:req.body.dlanguagesknown,
+        dpoliceverificationcertificate:req.body.dpoliceverificationcertificate,
+        ddrivinglicenceproof:req.body.ddrivinglicenceproof,
+        dpoliceverificationproof:req.body.dpoliceverificationproof,
+        dinsuranceproof:req.body.dinsuranceproof,
+        dfrontadharproof:req.body.dfrontadharproof,
+        dbackadharproof:req.body.dbackadharproof
+    }];
+    enrollfleetowner.bankdetails={
+        bbankname:req.body.bbankname,
+        baccountnumber:req.body.baccountnumber,
+        bbranchcode:req.body.bbranchcode,
+        bifsccode:req.body.bifsccode,
+        bpassbookphoto:req.body.bpassbookphoto
+    };
+      enrollfleetowner.save()
+        .then(() => res.json('Enrollfleetowner updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 router.route('/updatevehicle/:id').post((req,res)=>{
     Enrollfleetowner.findByIdAndUpdate(req.params.id,
         {$push: {vehicledetails:{ vvehiclemodel:req.body.vvehiclemodel,
@@ -144,6 +223,36 @@ router.route('/updatevehicle/:id').post((req,res)=>{
             console.log(err);
             }else{
                 res.json('enrollfleetvehicleadded')
+            }
+        }
+        )
+  });
+  router.route('/updatedriver/:id').post((req,res)=>{
+    Enrollfleetowner.findByIdAndUpdate(req.params.id,
+        {$push: {driverdetails:{ddrivername:req.body.ddrivername,
+          ddriverid:req.body.ddriverid,
+          ddgender:req.body.dgender,
+          dpermanentaddress:req.body.dpermanentaddress,
+          dpresentaddress:req.body.dpresentaddress,
+          ddateofbirth:req.body.ddateofbirth,
+          dmobilenumber:req.body.dmobilenumber,
+          demergencycontactnumber:req.body.demergencycontactnumber,
+          dadharcardnumber:req.body.dadharcardnumber,
+          dinsurancenumber:req.body.dinsurancenumber,
+          ddrivinglicence:req.body.ddrivinglicence,
+          dlanguagesknown:req.body.dlanguagesknown,
+          dpoliceverificationcertificate:req.body.dpoliceverificationcertificate,
+          ddrivinglicenceproof:req.body.ddrivinglicenceproof,
+          dpoliceverificationproof:req.body.dpoliceverificationproof,
+          dinsuranceproof:req.body.dinsuranceproof,
+          dfrontadharproof:req.body.dfrontadharproof,
+          dbackadharproof:req.body.dbackadharproof}}},
+        {safe: true, upsert: true},
+        function(err, doc) {
+            if(err){
+            console.log(err);
+            }else{
+                res.json('enrollfleetdriveradded')
             }
         }
         )

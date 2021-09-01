cbbbebe7e522252b1a82f376b1a71a275062628e
Raja R <rraja9513@gmail.com>
2021-09-01 13:04:49 +0530

    enrollfleet owner multer added  01/09/2021

--- routes/enrollfleetowner.js
+++ routes/enrollfleetowner.js
@@ -1,4 +1,29 @@
 const router=require('express').Router();
+const multer=require('multer');
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+      cb(null, Date.now() + file.originalname);  
+  }
+});
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
+// const upload=multer({dest:'uploads/'});
 let Enrollfleetowner=require('../models/enrollfleetowner.model');
 router.route('/').post((req, res) => {
     Enrollfleetowner.find()
